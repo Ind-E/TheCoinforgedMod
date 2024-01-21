430a6eecf16bce55e3c6f9e517d57126cd455618
Indi <30554197+Ind-E@users.noreply.github.com>
2024-01-21 15:32:55 -0800

    fixed basic cards

--- src/main/java/GamblerMod/character/Gambler.java
+++ src/main/java/GamblerMod/character/Gambler.java
@@ -81,14 +81,14 @@ public class Gambler extends CustomPlayer {
         ArrayList<String> retVal = new ArrayList<>();
         //List of IDs of cards for your starting deck.
         //If you want multiple of the same card, you have to add it multiple times.
+        retVal.add(GamblerMod.cards.StrikeGambler.ID);
+        retVal.add(GamblerMod.cards.StrikeGambler.ID);
+        retVal.add(GamblerMod.cards.StrikeGambler.ID);
         retVal.add(GamblerMod.cards.LuckyStrike.ID);
-        retVal.add(GamblerMod.cards.StrikeGambler.ID);
-        retVal.add(GamblerMod.cards.StrikeGambler.ID);
-        retVal.add(GamblerMod.cards.StrikeGambler.ID);
+        retVal.add(GamblerMod.cards.DefendGambler.ID);
+        retVal.add(GamblerMod.cards.DefendGambler.ID);
+        retVal.add(GamblerMod.cards.DefendGambler.ID);
         retVal.add(GamblerMod.cards.LuckyDefend.ID);
-        retVal.add(GamblerMod.cards.DefendGambler.ID);
-        retVal.add(GamblerMod.cards.DefendGambler.ID);
-        retVal.add(GamblerMod.cards.DefendGambler.ID);
         retVal.add(GamblerMod.cards.Advance.ID);
         retVal.add(GamblerMod.cards.AllIn.ID);
 
