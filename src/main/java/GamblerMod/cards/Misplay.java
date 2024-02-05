4e6ff4494056204b37cdcfdcb2e97de21ca10f34
Indi <30554197+Ind-E@users.noreply.github.com>
2024-02-04 16:50:40 -0800

    misplay--, roll instead of add

---
+++ src/main/java/GamblerMod/cards/Misplay.java
@@ -0,0 +1,33 @@
+package GamblerMod.cards;
+import com.megacrit.cardcrawl.actions.AbstractGameAction;
+import com.megacrit.cardcrawl.actions.common.DamageAction;
+import com.megacrit.cardcrawl.cards.DamageInfo;
+import com.megacrit.cardcrawl.characters.AbstractPlayer;
+import com.megacrit.cardcrawl.monsters.AbstractMonster;
+import GamblerMod.character.Gambler;
+import GamblerMod.util.CardStats;
+
+public class Misplay extends BaseCard{
+    private static final int DAMAGE = 30;
+    private static final int UPG_DAMAGE = 7;
+
+    public static final String ID = makeID(Misplay.class.getSimpleName());
+
+    private static final CardStats info = new CardStats(
+    Gambler.Enums.CARD_COLOR, 
+    CardType.ATTACK,
+    CardRarity.COMMON,
+    CardTarget.SELF_AND_ENEMY,
+    2
+    );
+
+    public Misplay() {
+        super(ID, info);
+        setDamage(DAMAGE, UPG_DAMAGE); 
+    }
+
+    @Override
+    public void use(AbstractPlayer p, AbstractMonster m) {
+        addToBot(new DamageAction(m, new DamageInfo(p, this.damage, DamageInfo.DamageType.NORMAL), AbstractGameAction.AttackEffect.NONE));
+    }
+}
