e4abc3b34f1bd7b7f96d63bac0140477b6034595
Indi <30554197+Ind-E@users.noreply.github.com>
2024-02-05 19:29:28 -0800

    full house

---
+++ src/main/java/GamblerMod/cards/FullHouse.java
@@ -0,0 +1,44 @@
+package GamblerMod.cards;
+
+import java.util.ArrayList;
+
+import com.megacrit.cardcrawl.actions.AbstractGameAction;
+import com.megacrit.cardcrawl.actions.common.DamageAction;
+import com.megacrit.cardcrawl.cards.AbstractCard;
+import com.megacrit.cardcrawl.cards.DamageInfo;
+import com.megacrit.cardcrawl.characters.AbstractPlayer;
+import com.megacrit.cardcrawl.monsters.AbstractMonster;
+import GamblerMod.character.Gambler;
+import GamblerMod.util.CardStats;
+
+public class FullHouse extends BaseCard{
+    private static final int DAMAGE = 6;
+    private static final int UPG_DAMAGE = 9;
+
+    public static final String ID = makeID(FullHouse.class.getSimpleName());
+    private static final CardStats info = new CardStats(
+        Gambler.Enums.CARD_COLOR, 
+        CardType.ATTACK,
+        CardRarity.COMMON,
+        CardTarget.ENEMY,
+        1
+    );
+
+    public FullHouse() {
+        super(ID, info);
+        setDamage(DAMAGE, UPG_DAMAGE); 
+    }
+
+    @Override
+    public void use(AbstractPlayer p, AbstractMonster m) {
+        ArrayList<String> types = new ArrayList<String>();
+        for (AbstractCard c : p.hand.group) {
+            if (!types.contains(c.type.toString())) {
+                types.add(c.type.toString());
+            }
+        }
+        for (int i = 0; i < types.size(); i++) {
+            addToBot(new DamageAction(m, new DamageInfo(p, this.damage), AbstractGameAction.AttackEffect.NONE));
+        }
+    }
+}
