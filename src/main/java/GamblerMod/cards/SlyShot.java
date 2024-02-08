491c3b5558044722ce5ff618a283a9f4c407d306
Indi <30554197+Ind-E@users.noreply.github.com>
2024-02-07 17:54:11 -0800

    sly shot

---
+++ src/main/java/GamblerMod/cards/SlyShot.java
@@ -0,0 +1,36 @@
+package GamblerMod.cards;
+
+import com.megacrit.cardcrawl.characters.AbstractPlayer;
+import com.megacrit.cardcrawl.monsters.AbstractMonster;
+
+import GamblerMod.actions.DamageLowestHealthEnemyAction;
+import GamblerMod.character.Gambler;
+import GamblerMod.util.CardStats;
+
+public class SlyShot extends BaseCard{
+    private static final int DAMAGE = 6;
+    private static final int MAGIC = 2;
+    private static final int UPG_MAGIC = 1;
+
+    public static final String ID = makeID(SlyShot.class.getSimpleName());
+    private static final CardStats info = new CardStats(
+        Gambler.Enums.CARD_COLOR, 
+        CardType.ATTACK,
+        CardRarity.COMMON,
+        CardTarget.ALL_ENEMY,
+        1
+    );
+
+    public SlyShot() {
+        super(ID, info);
+        setDamage(DAMAGE); 
+        setMagic(MAGIC, UPG_MAGIC);
+    }
+
+    @Override
+    public void use(AbstractPlayer p, AbstractMonster m) {
+        for (int i = 0; i < this.magicNumber; i++) {
+            addToBot(new DamageLowestHealthEnemyAction(this.damage));
+        }
+    }
+}
