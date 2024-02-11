8f1c71a1e791c8414a740503c86229c70817c069
Indi <30554197+Ind-E@users.noreply.github.com>
2024-02-11 04:33:33 -0800

    keyword implementation fixes

--- src/main/java/GamblerMod/cards/PocketPair.java
+++ src/main/java/GamblerMod/cards/PocketPair.java
@@ -1,8 +1,12 @@
 package GamblerMod.cards;
 
+import java.util.ArrayList;
+
+import com.badlogic.gdx.Gdx;
 import com.megacrit.cardcrawl.actions.common.MakeTempCardInHandAction;
 import com.megacrit.cardcrawl.cards.AbstractCard;
 import com.megacrit.cardcrawl.characters.AbstractPlayer;
+import com.megacrit.cardcrawl.helpers.CardLibrary;
 import com.megacrit.cardcrawl.monsters.AbstractMonster;
 
 import GamblerMod.actions.RollGreenAction;
@@ -10,9 +14,14 @@ import GamblerMod.character.Gambler;
 import GamblerMod.util.CardStats;
 import basemod.helpers.CardModifierManager;
 import modifiers.RetainModifier;
+import GamblerMod.GamblerMod;
 
 public class PocketPair extends BaseCard{
     private static final int DICE_TO_ROLL = 2;
+
+    private float rotationTimer;
+    private int previewIndex;
+    private ArrayList<AbstractCard> dupeListForPrev = new ArrayList<>();
 
     public static final String ID = makeID(PocketPair.class.getSimpleName());
     private static final CardStats info = new CardStats(
@@ -39,6 +48,38 @@ public class PocketPair extends BaseCard{
             CardModifierManager.addModifier(c1, new RetainModifier());
             CardModifierManager.addModifier(c2, new RetainModifier());
         }
-        
     }
+
+    private ArrayList<AbstractCard> getList() {
+        ArrayList<AbstractCard> myList = new ArrayList<>();
+        for (AbstractCard q : CardLibrary.getAllCards()) {
+            if (q.hasTag(GamblerMod.GREEN_DIE) && q.damage <= 6) {
+                AbstractCard r = q.makeCopy();
+                myList.add(r);
+            } 
+        } 
+        return myList;
+    }
+
+    public void update() {
+        super.update();
+        if (this.dupeListForPrev.isEmpty())
+            this.dupeListForPrev.addAll(getList()); 
+        if (this.hb.hovered)
+            if (this.rotationTimer <= 0.0F) {
+                this.rotationTimer = 1.5F;
+                if (this.dupeListForPrev.size() == 0) {
+                this.cardsToPreview = (AbstractCard)CardLibrary.cards.get("Madness");
+                } else {
+                this.cardsToPreview = this.dupeListForPrev.get(this.previewIndex);
+                } 
+                if (this.previewIndex == this.dupeListForPrev.size() - 1) {
+                this.previewIndex = 0;
+                } else {
+                this.previewIndex++;
+                } 
+            } else {
+                this.rotationTimer -= Gdx.graphics.getDeltaTime();
+            }  
+  }
 }
