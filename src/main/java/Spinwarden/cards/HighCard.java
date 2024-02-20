68ce1472b70acec0986bbe87b5f627f67bcfd710
Indi <30554197+Ind-E@users.noreply.github.com>
2024-02-19 16:18:25 -0800

    rework high card

--- src/main/java/Spinwarden/cards/HighCard.java
+++ src/main/java/Spinwarden/cards/HighCard.java
@@ -2,7 +2,7 @@ package Spinwarden.cards;
 
 import com.megacrit.cardcrawl.actions.AbstractGameAction.AttackEffect;
 import com.megacrit.cardcrawl.actions.common.DamageAction;
-import com.megacrit.cardcrawl.actions.common.DrawCardAction;
+import com.megacrit.cardcrawl.actions.common.ModifyDamageAction;
 import com.megacrit.cardcrawl.cards.AbstractCard;
 import com.megacrit.cardcrawl.cards.DamageInfo;
 import com.megacrit.cardcrawl.characters.AbstractPlayer;
@@ -12,30 +12,42 @@ import Spinwarden.character.SpinwardenCharacter;
 import Spinwarden.util.CardStats;
 
 public class HighCard extends BaseCard {
-    private static final int DAMAGE = 7;
-    private static final int UPG_DAMAGE = 6;
-    private static final int CARD_DRAW = 1;
+    private static final int DAMAGE = 12;
+    private static final int MAGIC = 12;
+    private static final int UPG_MAGIC = 6;
+    private int counter = 0;
 
     public static final String ID = makeID(HighCard.class.getSimpleName());
     private static final CardStats info = new CardStats(
             SpinwardenCharacter.Enums.CARD_COLOR,
             CardType.ATTACK,
-            CardRarity.COMMON,
+            CardRarity.UNCOMMON,
             CardTarget.ENEMY,
             1);
 
     public HighCard() {
         super(ID, info);
-        setDamage(DAMAGE, UPG_DAMAGE);
-        setMagic(CARD_DRAW);
+        setDamage(DAMAGE);
+        setMagic(MAGIC, UPG_MAGIC);
     }
 
     @Override
     public void use(AbstractPlayer p, AbstractMonster m) {
-        addToBot(new DamageAction(m,
-                new DamageInfo(p, this.damage, com.megacrit.cardcrawl.cards.DamageInfo.DamageType.NORMAL),
-                AttackEffect.NONE));
-        addToBot(new DrawCardAction(this.magicNumber));
+        if (counter % 2 == 0) {
+            addToBot(new DamageAction(m, new DamageInfo(p, this.damage, DamageInfo.DamageType.NORMAL),
+                    AttackEffect.NONE));
+            addToBot(new ModifyDamageAction(this.uuid, this.magicNumber));
+        }
+        counter++;
+    }
+
+    @Override
+    public void triggerOnGlowCheck() {
+        if (counter % 2 == 0) {
+            this.glowColor = AbstractCard.GOLD_BORDER_GLOW_COLOR.cpy();
+        } else {
+            this.glowColor = AbstractCard.BLUE_BORDER_GLOW_COLOR.cpy();
+        }
     }
 
     @Override
