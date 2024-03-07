19254c280331531923ab62bea0eaf37553e826b8
Indi <30554197+Ind-E@users.noreply.github.com>
2024-03-06 20:12:17 -0800

    big hand patch looks relatively ok

--- src/main/java/CoinforgedPackage/patches/BigHandPatch.java
+++ src/main/java/CoinforgedPackage/patches/BigHandPatch.java
@@ -26,10 +26,12 @@ public class BigHandPatch {
                 (__instance.group.get(0)).targetAngle += 7.0F;
                 (__instance.group.get(1)).target_y += 5.0F * Settings.scale;
                 (__instance.group.get(1)).targetAngle += 2.5F;
+
                 (__instance.group.get(2)).target_y += 7.5F * Settings.scale;
                 (__instance.group.get(3)).target_y += 10.0F * Settings.scale;
                 (__instance.group.get(7)).target_y += 10.0F * Settings.scale;
                 (__instance.group.get(8)).target_y += 7.5F * Settings.scale;
+
                 (__instance.group.get(9)).target_y += 5.0F * Settings.scale;
                 (__instance.group.get(9)).targetAngle -= 2.5F;
                 (__instance.group.get(10)).target_y -= 60.0F * Settings.scale;
@@ -38,74 +40,101 @@ public class BigHandPatch {
                     c.targetDrawScale = 0.628125F;
                 break;
             case 12:
-                (__instance.group.get(1)).target_y += 22.0F * Settings.scale;
-                (__instance.group.get(2)).target_y += 18.0F * Settings.scale;
-                (__instance.group.get(3)).target_y += 15.0F * Settings.scale;
-                (__instance.group.get(4)).target_y += 10.0F * Settings.scale;
-                (__instance.group.get(5)).target_y += 4.0F * Settings.scale;
-                (__instance.group.get(6)).target_y += 4.0F * Settings.scale;
-                (__instance.group.get(7)).target_y += 10.0F * Settings.scale;
-                (__instance.group.get(8)).target_y += 15.0F * Settings.scale;
-                (__instance.group.get(9)).target_y += 18.0F * Settings.scale;
-                (__instance.group.get(10)).target_y += 22.0F * Settings.scale;
+                for (int i = 2; i < 10; i++) {
+                    (__instance.group.get(i)).target_y += 30.0F * Settings.scale;
+                }
+                (__instance.group.get(0)).target_y -= 60.0F * Settings.scale;
+                (__instance.group.get(0)).targetAngle += 7.0F;
+                (__instance.group.get(1)).target_y -= 5.0F * Settings.scale;
+                (__instance.group.get(1)).targetAngle += 2.5F;
+                
+                // (__instance.group.get(2)).target_y += 7.5F * Settings.scale;
+                (__instance.group.get(3)).target_y += 5.0F * Settings.scale;
+                (__instance.group.get(9)).target_y -= 5.0F * Settings.scale;
+                // (__instance.group.get(9)).target_y -= 5.0F * Settings.scale;
+
+                (__instance.group.get(10)).target_y -= 5.0F * Settings.scale;
+                (__instance.group.get(10)).targetAngle -= 2.5F;
+                (__instance.group.get(11)).target_y -= 60.0F * Settings.scale;
+                (__instance.group.get(11)).targetAngle -= 7.0F;
                 for (AbstractCard c : __instance.group)
                     c.targetDrawScale = 0.61875F;
                 break;
             case 13:
-                (__instance.group.get(1)).target_y += 23.0F * Settings.scale;
-                (__instance.group.get(2)).target_y += 20.5F * Settings.scale;
-                (__instance.group.get(3)).target_y += 16.5F * Settings.scale;
-                (__instance.group.get(4)).target_y += 9.5F * Settings.scale;
-                (__instance.group.get(5)).target_y += 4.0F * Settings.scale;
-                (__instance.group.get(6)).target_y += 0.0F * Settings.scale;
-                (__instance.group.get(7)).target_y += 4.0F * Settings.scale;
-                (__instance.group.get(8)).target_y += 9.5F * Settings.scale;
-                (__instance.group.get(9)).target_y += 16.5F * Settings.scale;
-                (__instance.group.get(10)).target_y += 20.5F * Settings.scale;
-                (__instance.group.get(11)).target_y += 23.0F * Settings.scale;
+                for (int i = 2; i < 11; i++) {
+                    (__instance.group.get(i)).target_y += 30.0F * Settings.scale;
+                }
+                (__instance.group.get(0)).target_y -= 60.0F * Settings.scale;
+                (__instance.group.get(0)).targetAngle += 7.0F;
+                (__instance.group.get(1)).target_y -= 10.0F * Settings.scale;
+                (__instance.group.get(1)).targetAngle += 2.5F;
+
+                (__instance.group.get(2)).target_y -= 10.0F * Settings.scale;
+                (__instance.group.get(3)).target_y += 10.0F * Settings.scale;
+                (__instance.group.get(4)).target_y += 10.0F * Settings.scale;
+
+                (__instance.group.get(8)).target_y += 10.0F * Settings.scale;
+                (__instance.group.get(9)).target_y += 10.0F * Settings.scale;
+                (__instance.group.get(10)).target_y -= 10.0F * Settings.scale;
+
+                (__instance.group.get(11)).target_y -= 10.0F * Settings.scale;
+                (__instance.group.get(11)).targetAngle -= 2.5F;
+                (__instance.group.get(12)).target_y -= 60.0F * Settings.scale;
+                (__instance.group.get(12)).targetAngle -= 7.0F;
                 for (AbstractCard c : __instance.group)
                     c.targetDrawScale = 0.609375F;
                 break;
             case 14:
-                (__instance.group.get(1)).target_y += 19.0F * Settings.scale;
-                (__instance.group.get(2)).target_y += 17.0F * Settings.scale;
-                (__instance.group.get(3)).target_y += 16.0F * Settings.scale;
-                (__instance.group.get(4)).target_y += 15.0F * Settings.scale;
-                (__instance.group.get(5)).target_y += 10.0F * Settings.scale;
-                (__instance.group.get(6)).target_y += 4.0F * Settings.scale;
-                (__instance.group.get(7)).target_y += 4.0F * Settings.scale;
-                (__instance.group.get(8)).target_y += 10.0F * Settings.scale;
-                (__instance.group.get(9)).target_y += 15.0F * Settings.scale;
-                (__instance.group.get(10)).target_y += 16.0F * Settings.scale;
-                (__instance.group.get(11)).target_y += 17.0F * Settings.scale;
-                (__instance.group.get(12)).target_y += 19.0F * Settings.scale;
+                for (int i = 2; i < 12; i++) {
+                    (__instance.group.get(i)).target_y += 30.0F * Settings.scale;
+                }
+                (__instance.group.get(0)).target_y -= 60.0F * Settings.scale;
+                (__instance.group.get(0)).targetAngle += 7.0F;
+                (__instance.group.get(1)).target_y -= 10.0F * Settings.scale;
+                (__instance.group.get(1)).targetAngle += 2.5F;
+
+                (__instance.group.get(2)).target_y -= 20.0F * Settings.scale;
+                (__instance.group.get(3)).target_y += 0.0F * Settings.scale;
+                (__instance.group.get(11)).target_y -= 20.0F * Settings.scale;
+
+                (__instance.group.get(12)).target_y -= 10.0F * Settings.scale;
+                (__instance.group.get(12)).targetAngle -= 2.5F;
+                (__instance.group.get(13)).target_y -= 60.0F * Settings.scale;
+                (__instance.group.get(13)).targetAngle -= 7.0F;
                 for (AbstractCard c : __instance.group)
                     c.targetDrawScale = 0.6F;
                 break;
             case 15:
-                (__instance.group.get(0)).target_y -= 50.0F * Settings.scale;
-                (__instance.group.get(0)).targetAngle += 10.0F;
-                (__instance.group.get(1)).target_y -= 25.0F * Settings.scale;
-                (__instance.group.get(1)).targetAngle += 10.0F;
+                for (int i = 2; i < 13; i++) {
+                    (__instance.group.get(i)).target_y += 35.0F * Settings.scale;
+                }
+                (__instance.group.get(0)).target_y -= 60.0F * Settings.scale;
+                (__instance.group.get(0)).targetAngle += 7.0F;
+                (__instance.group.get(1)).target_y -= 15.0F * Settings.scale;
+                (__instance.group.get(1)).targetAngle += 2.5F;
+
                 (__instance.group.get(2)).target_y -= 10.0F * Settings.scale;
-                (__instance.group.get(3)).targetAngle += 2.0F;
-                (__instance.group.get(3)).target_y -= 5.0F * Settings.scale;
-                (__instance.group.get(4)).target_y += 15.0F * Settings.scale;
-                (__instance.group.get(5)).target_y += 20.0F * Settings.scale;
-                (__instance.group.get(6)).target_y += 15.0F * Settings.scale;
-                (__instance.group.get(7)).target_y += 15.0F * Settings.scale;
-                (__instance.group.get(8)).target_y += 15.0F * Settings.scale;
-                (__instance.group.get(9)).target_y += 20.0F * Settings.scale;
-                (__instance.group.get(10)).target_y += 15.0F * Settings.scale;
-                (__instance.group.get(11)).target_y -= 5.0F * Settings.scale;
+                (__instance.group.get(2)).targetAngle += 1.5F;
+                (__instance.group.get(3)).target_y += 0.0F * Settings.scale;
+                (__instance.group.get(3)).targetAngle -= 0.5F;
+                (__instance.group.get(4)).target_y += 10.0F * Settings.scale;  
+                (__instance.group.get(5)).target_y += 10.0F * Settings.scale;
+
+                (__instance.group.get(7)).target_y -= 3.0F * Settings.scale;
+
+                (__instance.group.get(9)).target_y += 10.0F * Settings.scale;              
+                (__instance.group.get(10)).target_y += 10.0F * Settings.scale;
+                (__instance.group.get(11)).target_y += 0.0F * Settings.scale;
+                (__instance.group.get(11)).targetAngle += 0.5F;
                 (__instance.group.get(12)).target_y -= 10.0F * Settings.scale;
-                (__instance.group.get(12)).targetAngle -= 2.0F;
-                (__instance.group.get(13)).target_y -= 25.0F * Settings.scale;
-                (__instance.group.get(13)).targetAngle -= 10.0F;
-                (__instance.group.get(14)).target_y -= 50.0F * Settings.scale;
-                (__instance.group.get(14)).targetAngle -= 10.0F;
+                (__instance.group.get(12)).targetAngle -= 1.5F;
+
+                (__instance.group.get(13)).target_y -= 15.0F * Settings.scale;
+                (__instance.group.get(13)).targetAngle -= 2.5F;
+                (__instance.group.get(14)).target_y -= 60.0F * Settings.scale;
+                (__instance.group.get(14)).targetAngle -= 7.0F;
                 for (AbstractCard c : __instance.group)
-                    c.targetDrawScale = 0.590625F;
+                    c.targetDrawScale = 0.6F;
                 break;
         }
     }
