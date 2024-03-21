7c7efc17e95f546eba11f04d7b4eb95a72dbc700
Indi <30554197+Ind-E@users.noreply.github.com>
2024-03-20 23:41:21 -0700

    reveal card_data

--- src/main/java/CoinforgedPackage/patches/PokerChipsNoUpgradePatch.java
+++ src/main/java/CoinforgedPackage/patches/PokerChipsNoUpgradePatch.java
@@ -6,14 +6,13 @@ import com.megacrit.cardcrawl.cards.AbstractCard;
 import com.evacipated.cardcrawl.modthespire.lib.SpirePrefixPatch;
 import com.evacipated.cardcrawl.modthespire.lib.SpireReturn;
 
-import CoinforgedPackage.cards.AbstractCoinforgedCard;
 import CoinforgedPackage.cards.CustomTags;
 
 @SpirePatch(clz = SingleCardViewPopup.class, method = "allowUpgradePreview")
 public class PokerChipsNoUpgradePatch {
     @SpirePrefixPatch
     public static SpireReturn<Boolean> Prefix(SingleCardViewPopup __instance, AbstractCard ___card) {
-        if (___card.hasTag(CustomTags.POKER_CHIP) || ___card instanceof AbstractCoinforgedCard && ___card.upgraded) {
+        if (___card.hasTag(CustomTags.POKER_CHIP)) {
             return SpireReturn.Return(false);
         }
         return SpireReturn.Continue();
