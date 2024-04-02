2cff1555740a1161c91cc2801635d7776fb92595
Indi <30554197+Ind-E@users.noreply.github.com>
2024-04-01 23:45:32 -0700

    added images for melonPower and advantagePower

---
+++ src/main/java/CoinforgedPackage/cards/RedRage.java
@@ -0,0 +1,35 @@
+package CoinforgedPackage.cards;
+
+import com.megacrit.cardcrawl.actions.common.ApplyPowerAction;
+import com.megacrit.cardcrawl.characters.AbstractPlayer;
+import com.megacrit.cardcrawl.monsters.AbstractMonster;
+import CoinforgedPackage.util.CardStats;
+import CoinforgedPackage.actions.RollRedAction;
+import CoinforgedPackage.character.Coinforged;
+import CoinforgedPackage.powers.AdvantagePower;
+
+public class RedRage extends AbstractCoinforgedCard {
+    private static final int ROLLS = 3;
+    private static final int UPG_ROLLS = 1;
+    private static final int MAGIC = 1;
+
+    public static final String ID = makeID(RedRage.class.getSimpleName());
+    private static final CardStats info = new CardStats(
+            Coinforged.Enums.CARD_COLOR,
+            CardType.SKILL,
+            CardRarity.COMMON,
+            CardTarget.NONE,
+            1);
+
+    public RedRage() {
+        super(ID, info);
+        setMagic(MAGIC);
+        setCustomVar("R", ROLLS, UPG_ROLLS);
+    }
+
+    @Override
+    public void use(AbstractPlayer p, AbstractMonster m) {
+        addToBot(new RollRedAction(p, customVar("R")));
+        addToBot(new ApplyPowerAction(p, p, new AdvantagePower(p, this.magicNumber)));
+    }
+}
